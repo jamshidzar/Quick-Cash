1641e7c71d4b69ca70c6ee2708a639ea176aef0e
Tanishika <tn900654@dal.ca>
2024-09-26 11:30:14 -0300

    Initial push to branch Tanishika

---
+++ Registration/app/src/androidTest/java/UserLoginTest/UserLoginEspressotest.java
@@ -0,0 +1,26 @@
+package UserLoginTest;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import static org.junit.Assert.assertTrue;
+
+import android.util.Log;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+@RunWith(AndroidJUnit4.class)
+public class UserLoginEspressotest {
+    @Test
+    public void dummyTest(){
+        assertTrue(2 == 2);
+    }
+
+}
