26502a2582207b218336ee8841d1a6f6c8898aab
kmatheson <kv686416@dal.ca>
2024-10-02 23:06:56 -0300

    Set up firestore db and allowed for data to be captured in db upon hitting registration button

--- Registration/app/src/main/java/com/example/registration/MainActivity.java
+++ Registration/app/src/main/java/com/example/registration/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.registration;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
+    EditText name, email, password;
+    Button Registration;
+
+    FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +37,38 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        db = FirebaseFirestore.getInstance();
+        name = findViewById(R.id.NameText);
+        email = findViewById(R.id.EmailText);
+        password = findViewById(R.id.PasswordText);
+        Registration = findViewById(R.id.RegisterButton);
+        Registration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                String Name = name.getText().toString();
+                String Email = email.getText().toString();
+                String Password = password.getText().toString();
+
+                Map<String, Object> user = new HashMap<>();
+                user.put("Name", Name);
+                user.put("Email", Email);
+                user.put("Password", Password);
+
+                db.collection("user").add(user)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Toast.makeText(MainActivity.this, "Registration Successful", Toast.LENGTH_LONG).show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(MainActivity.this, "Registration Failed", Toast.LENGTH_LONG).show();
+                            }
+                        });
+            }
+        });
     }
+
+
 }
