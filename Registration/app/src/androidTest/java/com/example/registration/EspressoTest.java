84f8634570312c84c722bb90c2a0beb8d7130782
kmatheson <kv686416@dal.ca>
2024-10-09 20:22:01 -0300

    added a couple more espresso tests for checking invalid credentials. All are passing.

--- Registration/app/src/androidTest/java/com/example/registration/EspressoTest.java
+++ Registration/app/src/androidTest/java/com/example/registration/EspressoTest.java
@@ -78,5 +78,46 @@ public class EspressoTest {
         onView(withId(R.id.RegisterButton)).perform(click());
         onView(withId(R.id.errorMSG)).check(matches(withText("")));
     }
+
+    @Test
+    public void checkIfInvalidName(){
+        onView(withId(R.id.NameText)).perform(typeText("B"), closeSoftKeyboard());
+        onView(withId(R.id.EmailText)).perform(typeText("bob@gmail.com"), closeSoftKeyboard());
+        onView(withId(R.id.PasswordText)).perform(typeText("Password1!"), closeSoftKeyboard());
+        onView(withId(R.id.CreditCard)).perform(typeText("1111222233334444"), closeSoftKeyboard());
+        onView(withId(R.id.RegisterButton)).perform(click());
+        onView(withId(R.id.errorMSG)).check(matches(withText("Invalid Name")));
+    }
+
+    @Test
+    public void checkIfInvalidEmail(){
+        onView(withId(R.id.NameText)).perform(typeText("Bob Joe"), closeSoftKeyboard());
+        onView(withId(R.id.EmailText)).perform(typeText("bob@gmail.com.com"), closeSoftKeyboard());
+        onView(withId(R.id.PasswordText)).perform(typeText("Password1!"), closeSoftKeyboard());
+        onView(withId(R.id.CreditCard)).perform(typeText("1111222233334444"), closeSoftKeyboard());
+        onView(withId(R.id.RegisterButton)).perform(click());
+        onView(withId(R.id.errorMSG)).check(matches(withText("Invalid Email")));
+    }
+
+    @Test
+    public void checkIfInvalidPassword(){
+        onView(withId(R.id.NameText)).perform(typeText("Bob Joe"), closeSoftKeyboard());
+        onView(withId(R.id.EmailText)).perform(typeText("bob@gmail.com"), closeSoftKeyboard());
+        onView(withId(R.id.PasswordText)).perform(typeText("password"), closeSoftKeyboard());
+        onView(withId(R.id.CreditCard)).perform(typeText("1111222233334444"), closeSoftKeyboard());
+        onView(withId(R.id.RegisterButton)).perform(click());
+        onView(withId(R.id.errorMSG)).check(matches(withText("Invalid Password")));
+    }
+
+    @Test
+    public void checkIfInvalidCreditCard(){
+        onView(withId(R.id.NameText)).perform(typeText("Bob Joe"), closeSoftKeyboard());
+        onView(withId(R.id.EmailText)).perform(typeText("bob@gmail.com"), closeSoftKeyboard());
+        onView(withId(R.id.PasswordText)).perform(typeText("Password1!"), closeSoftKeyboard());
+        onView(withId(R.id.CreditCard)).perform(typeText("1111222233334"), closeSoftKeyboard());
+        onView(withId(R.id.RegisterButton)).perform(click());
+        onView(withId(R.id.errorMSG)).check(matches(withText("Invalid Credit Card")));
+    }
+
     
 }
