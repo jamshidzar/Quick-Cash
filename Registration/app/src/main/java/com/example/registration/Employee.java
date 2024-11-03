ec4f0d1b9d2477e00f2ce97afb72641ee11eb832
Tanishika <tn900654@dal.ca>
2024-11-03 14:33:26 -0400

    resolved conflicts after merging with demo

--- Registration/app/src/main/java/com/example/registration/Employee.java
+++ Registration/app/src/main/java/com/example/registration/Employee.java
@@ -6,15 +6,8 @@ import android.widget.Toast;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
-
-
-import android.widget.TextView;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -118,7 +111,7 @@ public class Employee extends AppCompatActivity {
 
     }
 
-}
+
     public void enableJobAlerts() {
         CheckBox checkBox = findViewById(R.id.checkBox);
         checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
